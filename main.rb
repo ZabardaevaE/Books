67aea79bce19e2e57aa90acc9656ecad7b228ab4
evgeniya <zena28@gmail.com>
2022-09-13 16:06:14 +0930

    Edit and Delete functions added

--- main.rb
+++ main.rb
@@ -32,6 +32,36 @@ post '/books' do
   redirect '/'
 end
 
+get '/books/:id/edit' do
+  id = params['id']
+  book = run_sql("SELECT * FROM books WHERE id = $1", [id])[0]
+
+  erb :'books/edit', locals: {
+    book: book
+  }
+end
+
+put '/books/:id' do
+  id = params['id']
+  name = params['name']
+  image_url = params['image_url']
+  author = params['author']
+  genre = params['genre']
+  year = params['year']
+ 
+
+
+  run_sql("UPDATE books SET name = $2, image_url = $3, author = $4, genre = $5, year = $6 WHERE id = $1", [id,name, image_url, author, genre, year])
+  redirect '/'
+end
+
+delete '/books/:id' do
+  id = params['id']
+  run_sql("DELETE FROM books WHERE id = $1", [id])
+  redirect '/'
+end
+
+
 
 
 
