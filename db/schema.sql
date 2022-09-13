d8332c893d6d4b61757a26787e7385a35b94bbfa
evgeniya <zena28@gmail.com>
2022-09-13 15:19:38 +0930

    Database created, Add new book button created

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE books_db;
+\c books_db
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    author TEXT,
+    image_url TEXT,
+    genre TEXT,
+    year INTEGER
+);
+
+INSERT INTO books(name, author, image_url, genre, year)
+VALUES('The Mousetrap', 'Agatha Christie','https://images.unsplash.com/photo-1518380817578-3e32a02eea74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YWdhdGhhJTIwY2hyaXN0aWUlMjBtb3VzZXRyYXB8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60', 'crime fiction', 1952);
+
+INSERT INTO books(name, author, image_url, genre, year)
+VALUES('The Great Gatsby', 'F. Scott Fitzgerald', 'https://images.unsplash.com/photo-1615413833480-6e8427dbcc5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGhlJTIwZ3JlYXQlMjBnYXRzYnl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60', 'tragedy', 1925);
--- main.rb
+++ main.rb
@@ -1,9 +1,41 @@
-     
+
 require 'sinatra'
+require 'pg'
+
+def run_sql(sql, sql_params = [])
+  db = PG.connect(dbname: 'books_db')
+  results = db.exec_params(sql, sql_params)
+  db.close
+  results
+end
 
 get '/' do
-  erb :index
+  books = run_sql("SELECT * FROM books")
+
+  erb :'books/index', locals: {
+    books: books
+  }
 end
+
+get '/books/new' do
+  erb :'books/new'
+end
+
+post '/books' do
+  name = params['name']
+  image_url = params['image_url']
+  author = params['author']
+  genre = params['genre']
+  year = params['year']
+  run_sql("INSERT INTO books(name, image_url, author, genre, year) VALUES($1, $2, $3, $4, $5)", [name, image_url, author, genre, year])
+
+  redirect '/'
+end
+
+
+
+
+    
 
 
 
