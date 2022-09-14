6760f5060c5bd712e942ac782cb7ee9c140c683c
evgeniya <zena28@gmail.com>
2022-09-14 14:52:08 +0930

    individual pages for users added

--- controllers/books_controller.rb
+++ controllers/books_controller.rb
@@ -1,10 +1,67 @@
+# require './models/book'
+
+# get '/' do
+#     books = all_books()
+  
+#     erb :'books/index', locals: {
+#       books: books
+#     }
+#   end
+  
+#   get '/books/new' do
+#     erb :'books/new'
+#   end
+  
+#   post '/books' do
+#     name = params['name']
+#     image_url = params['image_url']
+#     author = params['author']
+#     genre = params['genre']
+#     year = params['year']
+    
+#     create_book(name, image_url, author, genre, year)
+  
+#     # if the request is NOT a GET request, then we must redirect instead of using erb.
+#     redirect '/'
+#   end
+  
+#   get '/books/:id/edit' do
+#     id = params['id']
+#     book = get_book(id)
+  
+#     erb :'books/edit', locals: {
+#       book: book
+#     }
+#   end
+  
+#   put '/books/:id' do
+#     id = params['id']
+#     name = params['name']
+#     image_url = params['image_url']
+#     author = params['author']
+#     genre = params['genre']
+#     year = params['year']
+
+#     update_book(id, name, image_url, author, genre, year)
+#     redirect '/'
+#   end
+  
+#   delete '/books/:id' do
+#     id = params['id']
+    
+#     delete_book(id)
+#     redirect '/'
+#   end
+
 require './models/book'
 
 get '/' do
-    books = all_books()
+    # books = all_books()
+    owner_id = session['user_id']
+    logged_books = book_owner(owner_id)
   
     erb :'books/index', locals: {
-      books: books
+      books: logged_books
     }
   end
   
@@ -18,8 +75,9 @@ get '/' do
     author = params['author']
     genre = params['genre']
     year = params['year']
+    owner_id = session['user_id']
     
-    create_book(name, image_url, author, genre, year)
+    create_book(name, image_url, author, genre, year, owner_id)
   
     # if the request is NOT a GET request, then we must redirect instead of using erb.
     redirect '/'
@@ -52,3 +110,5 @@ get '/' do
     delete_book(id)
     redirect '/'
   end
+
+  
--- models/book.rb
+++ models/book.rb
@@ -1,18 +1,43 @@
+# def all_books
+#     run_sql("SELECT * FROM books ORDER BY id")
+# end
+
+# def create_book(name, image_url, author, genre, year)
+#     run_sql("INSERT INTO books(name, image_url, author, genre, year) VALUES($1, $2, $3, $4, $5)", [name, image_url, author, genre, year])
+#   end
+#   def get_book(id)
+#     run_sql("SELECT * FROM books WHERE id = $1", [id])[0]
+#   end
+
+#   def update_book(id,name, image_url, author, genre, year)
+#     run_sql("UPDATE books SET name = $2, image_url = $3, author = $4, genre = $5, year = $6 WHERE id = $1", [id,name, image_url, author, genre, year])
+# end
+
+#   def delete_book(id)
+#     run_sql("DELETE FROM books WHERE id = $1", [id])
+#   end
+
 def all_books
     run_sql("SELECT * FROM books ORDER BY id")
 end
 
-def create_book(name, image_url, author, genre, year)
-    run_sql("INSERT INTO books(name, image_url, author, genre, year) VALUES($1, $2, $3, $4, $5)", [name, image_url, author, genre, year])
+def create_book(name, image_url, author, genre,   year, owner)
+    run_sql("INSERT INTO books(name, image_url, author, genre, year, owner) VALUES($1, $2, $3, $4, $5, $6)", [name, image_url, author, genre, year, owner])
   end
-  def get_book(id)
+def get_book(id)
     run_sql("SELECT * FROM books WHERE id = $1", [id])[0]
-  end
+end
 
-  def update_book(id,name, image_url, author, genre, year)
+def update_book(id,name, image_url, author, genre, year)
     run_sql("UPDATE books SET name = $2, image_url = $3, author = $4, genre = $5, year = $6 WHERE id = $1", [id,name, image_url, author, genre, year])
 end
 
-  def delete_book(id)
+def delete_book(id)
     run_sql("DELETE FROM books WHERE id = $1", [id])
-  end
+end
+
+def book_owner(owner_id)
+    run_sql("SELECT * FROM books WHERE owner = $1", [owner_id])
+end    
+
+  
